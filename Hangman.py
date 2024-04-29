50e8a072d02b46aa8558c9f5e03cb55a7702e416
[James Thomas] <[James.Thomas@paccar.com]>
2024-04-29 09:11:17 -0700

    So slow

--- Hangman.py
+++ Hangman.py
@@ -277,7 +277,7 @@ while True:
         firstTime = False
 
         trashTalkList = ["Stretch his neck boys!", "Necktie party time!!", "Watch him jump!", "You should have chosen another letter", 
-                         "You will pay for that!!", "Try again"]
+                         "You will pay for that!!", "So slow..."]
         randomPhrase = (random.choice(trashTalkList))
         
                 # Choose a word... 
